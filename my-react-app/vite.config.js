884d36b4a0b108f886068c450765777bdc9cc7db
andrewtan1018 <tan434@purdue.edu>
2025-11-15 14:35:44 -0500

    Fixed tailwind issue

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-// import tailwindcss from "@taiwindcss/vite"
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/",
-})
+  plugins: [react(), tailwindcss()],
+  base: "/", // replace with your repo name; use "/" for USERNAME.github.io
+});
